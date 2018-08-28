ad8d6b2ecd0f86ed2aa60ad3ae9b6ab29842023b
Alfonso <plutoyem@outlook.com>
2018-08-27 21:52:10 -0400

    leetcode-142

---
+++ 142LinkedListCycleII.py
@@ -0,0 +1,55 @@
+"""
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+Note: Do not modify the linked list.
+
+Follow up:
+Can you solve it without using extra space?
+"""
+"""
+Comments
+"""
+"""
+My
+"""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        nodes = set([])
+        node = head
+        nodes.add(node)
+        while node is not None:
+            if node.next in nodes:
+                return node.next
+            nodes.add(node)
+            node = node.next
+        return None
+"""
+Fast
+"""
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow, fast = head, head
+        
+        while fast and fast.next:
+            fast, slow = fast.next.next, slow.next
+            if fast is slow:
+                fast = head
+                while fast is not slow:
+                    fast, slow = fast.next, slow.next
+
+                return fast
+        
+        return None
