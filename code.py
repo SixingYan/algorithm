1b2dd7725abb9a4f7360a62378012f9825e847a8
Alfonso <plutoyem@outlook.com>
2018-11-02 19:10:28 -0400

    leetcode904 easy beat80%

--- code.py
+++ code.py
@@ -9,35 +9,28 @@ class Solution(object):
 
 
     def function():
-        pass
+        
 
 
-        # visit around
-        lists = [l for l in lists if l is not None]
-        if len(lists) == 1:
-            return lists[0]
-        if len(lists) == 0:
-            return None
-        vd = {}
-        for i in range(len(lists)): 
-            lst = lists[i]
-            while lst is not None:
-                ky = lst.val
-                if not ky in vd.keys():
-                    vd[ky] = [lst, lst]
-                else:
-                    vd[ky][1].next = lst
-                    vd[ky][1] = vd[ky][1].next
-                lst = lst.next
-                vd[ky][1].next = None
 
-        vl = sorted(list(vd.items()),key=lambda v: v[0])
-        head = vl[0][1][0]
-        for i in range(1, len(vl)):
-            t = vl[i-1][1][1]
-            t.next = vl[i][1][0]
-            t = t.next
-        return head
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
