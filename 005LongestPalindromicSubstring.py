531b127fdf453aaae2f9a0775bcab436e66dab78
Alfonso <plutoyem@outlook.com>
2019-04-06 18:50:22 -0400

    add comments

--- 005LongestPalindromicSubstring.py
+++ 005LongestPalindromicSubstring.py
@@ -18,24 +18,27 @@ Comments
 """
 My
 """
+
+
 class Solution:
+
     def longestPalindrome(self, s: str) -> str:
         count = []
         for i in range(len(s)):
-            if i>0 and s[i-1] == s[i]:
+            if i > 0 and s[i - 1] == s[i]:
                 c = 0
-                while i-c-1 > -1 and i+c < len(s):
-                    if s[i-c-1] != s[i+c]:
+                while i - c - 1 > -1 and i + c < len(s):
+                    if s[i - c - 1] != s[i + c]:
                         break
                     c += 1
-                count.append(s[i-1-c+1:i+c])
-            if i>1 and s[i-2] == s[i]:
+                count.append(s[i - 1 - c + 1:i + c])
+            if i > 1 and s[i - 2] == s[i]:
                 c = 0
-                while i-c-2 > -1 and i+c < len(s):
-                    if s[i-c-2] != s[i+c]:
+                while i - c - 2 > -1 and i + c < len(s):
+                    if s[i - c - 2] != s[i + c]:
                         break
                     c += 1
-                count.append(s[i-2-c+1:i+c])
+                count.append(s[i - 2 - c + 1:i + c])
         if len(count) == 0:
             if len(s) > 0:
                 return s[0]
@@ -49,10 +52,12 @@ class Solution:
 """
 Fast
 """
+
+
 class Solution:
     head = 0
     max_length = 0
-    
+
     def longestPalindrome(self, s):
         """
         :type s: str
@@ -60,12 +65,12 @@ class Solution:
         """
         if len(s) < 2:
             return s
-        
+
         for idx in range(len(s) - 1):
             self.search_palindrome(s, idx, idx)
             self.search_palindrome(s, idx, idx + 1)
         return s[self.head:self.head + self.max_length]
-            
+
     def search_palindrome(self, s, left, right):
         while left >= 0 and right < len(s) and s[left] == s[right]:
             left -= 1
